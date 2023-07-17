06a2bcc7c8e981c600e96e8d03d465d1f1e05ecb
Rebeca Beatriz de Luna <122133374+rebecabeatrizdeluna@users.noreply.github.com>
2023-07-17 11:45:36 +0000

    Adição de mais um gráfico

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import yfinance as yf
 import plotly.graph_objects as go 
+import numpy as np
 
 # Título do App 
 st.title('Stock History App')
@@ -20,4 +21,22 @@ st.dataframe(data)
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=data.index, y = data['Close'], name = 'fechamento'))
 fig.update_layout(title = f"{ticker_symbol}", xaxis_title = "Data", yaxis_title = "Preço")
+st.plotly_chart(fig)
+
+# Implementando RSI
+data['PriceDiff'] = data['Close'].diff()
+
+data['Gain'] = np.where(data['PriceDiff'] > 0, data['PriceDiff'], 0)
+data['Loss'] = np.where(data['PriceDiff'] < 0, abs(data['PriceDiff']), 0)
+
+data['AvgGain'] = data['Gain'].rolling(window=30).mean()
+data['AvgLoss'] = data['Loss'].rolling(window=30).mean()
+
+data['RS'] = data['AvgGain'] / data['AvgLoss']
+data['RSI'] = 100 - (100 / (1 + data['RS']))
+
+# Exibir gráfico do RSI
+fig=go.Figure()
+fig.add_trace(go.Scatter(x=data.index,y= data['RSI'],name= 'Fechamento'))
+fig.update_layout(title=f"Preço do ativo {ticker_symbol}",xaxis_title = "Date", yaxis_title = "Preço")
 st.plotly_chart(fig)
